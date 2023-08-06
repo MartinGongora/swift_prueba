8c98c08cb462260df2fbf4aff9732e83e3758db5
MartinGongora <martin_gongora@hotmail.com>
2023-08-05 21:38:29 -0300

    agrego Navigation view

--- swift_prueba/ContentView.swift
+++ swift_prueba/ContentView.swift
@@ -18,20 +18,17 @@ struct Photo {
 
 let photos = [
     Photo(id: "1", author: "yo", width: 300, heigth: 300, url: "algo.com", download_url: "algo.com"),
-    Photo(id: "1", author: "yo", width: 300, heigth: 300, url: "algo.com", download_url: "algo.com"),
-    Photo(id: "1", author: "yo", width: 300, heigth: 300, url: "algo.com", download_url: "algo.com"),
-    Photo(id: "1", author: "yo", width: 300, heigth: 300, url: "algo.com", download_url: "algo.com"),
+    Photo(id: "2", author: "yo", width: 300, heigth: 300, url: "algo.com", download_url: "algo.com"),
+    Photo(id: "3", author: "yo", width: 300, heigth: 300, url: "algo.com", download_url: "algo.com"),
+    Photo(id: "4", author: "yo", width: 300, heigth: 300, url: "algo.com", download_url: "algo.com"),
 ]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Listado")
-                .padding()
-                .font(.largeTitle)
+        NavigationView {
             List{
                 ForEach(photos, id: \.id){ photo in
-                    Label(photo.id, systemImage: photo.url)
+                    Text(photo.id + " " + photo.author)
                 }
             }
         }
